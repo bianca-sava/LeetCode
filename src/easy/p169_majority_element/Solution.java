07a35c6e6d059fe7c8601a02feb838f2ca44adc0
Bianca Sava <biancasava07@gmail.com>
2026-02-19 10:24:22 +0200

    p169

---
+++ src/easy/p169_majority_element/Solution.java
@@ -0,0 +1,14 @@
+package easy.p169_majority_element;
+
+public class Solution {
+    public int majorityElement(int[] nums) {
+        int candidate = -1;
+        int count = 0;
+        for(int i =0 ;i<nums.length;i++){
+            if(count == 0) candidate = nums[i];
+            if(candidate == nums[i]) count++;
+            else count --;
+        }
+        return candidate;
+    }
+}
