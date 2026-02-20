9d39f31da8eeade9a5a44c0240e19cafc5fab9f8
Bianca Sava <biancasava07@gmail.com>
2026-02-20 15:33:45 +0200

    p50

---
+++ src/medium/p50_pow/Solution.java
@@ -0,0 +1,24 @@
+package medium.p50_pow;
+
+public class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        double result = 1;
+        double current_product = x;
+
+        while (N > 0) {
+            if (N % 2 == 1) {
+                result = result * current_product;
+            }
+            current_product = current_product * current_product;
+            N = N / 2;
+        }
+
+        return result;
+    }
+}
