277235a0d3acee7fd92604f73d859ee4c11c0945
Bianca Sava <biancasava07@gmail.com>
2026-02-20 15:27:00 +0200

    p202

---
+++ src/easy/p202_happy_number/Solution.java
@@ -0,0 +1,27 @@
+package easy.p202_happy_number;
+
+public class Solution {
+    public int square(int n){
+        int res = 0;
+        while(n>0){
+            res += (n%10)*(n%10);
+            n/=10;
+        }
+
+        return res;
+    }
+
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+        do{
+            slow = square(slow);
+            fast = square(square(fast));
+        }
+        while(slow!=fast);
+
+        return slow == 1;
+    }
+
+
+}
