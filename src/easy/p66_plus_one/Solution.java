4a9302da2d538c4bb6c858bb72215e2c0b7a4535
Bianca Sava <biancasava07@gmail.com>
2026-02-23 14:42:59 +0200

    p66

---
+++ src/easy/p66_plus_one/Solution.java
@@ -0,0 +1,20 @@
+package easy.p66_plus_one;
+
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+
+        for(int i =n-1; i>=0; i--){
+            if(digits[i]<9){
+                digits[i]+=1;
+                return digits;
+            }
+            digits[i]=0;
+        }
+
+        int[] res = new int[n+1];
+        res[0]= 1;
+
+        return res;
+    }
+}
