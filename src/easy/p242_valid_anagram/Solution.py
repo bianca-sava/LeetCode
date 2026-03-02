9c5dd6d5dfde71eaee17ad4c93b7c0060be090b2
Bianca Sava <biancasava07@gmail.com>
2026-03-02 12:17:51 +0200

    p242

---
+++ src/easy/p242_valid_anagram/Solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
+
+        hm = {}
+        for i in range(len(s)):
+            if s[i] in hm:
+                hm[s[i]] += 1
+            else:
+                hm[s[i]] = 1
+
+        for i in range(len(t)):
+            if t[i] in hm:
+                hm[t[i]] -= 1
+            else:
+                return False
+
+        for val in hm.values():
+            if val != 0:
+                return False
+
+        return True
