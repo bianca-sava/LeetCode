e8470dea6a82d3e777219723074f27c8de1d15a3
Bianca Sava <biancasava07@gmail.com>
2026-03-02 10:32:09 +0200

    p392

---
+++ src/easy/p392_is_subsequence/Solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        p_s = 0
+        p_t = 0
+
+        while p_t < len(t) and p_s < len(s):
+            if s[p_s] == t[p_t]:
+                p_s += 1
+            p_t += 1
+
+        return p_s == len(s)
